1f5c3b7c83a982e216a51f6d42162088d4add3e2
VS Code <dev.swarup@hotmail.com>
2025-03-21 13:04:08 +0530

    Updated `logger`

--- src/js/log.js
+++ src/js/log.js
@@ -8,24 +8,7 @@ const colors = require("colors/safe"), GetTime = t => {
 
 colors.setTheme({ BLUE_BOLD: ["bold", "bgBlue"], CYAN_BOLD: ["bold", "bgCyan"], WHITE_BOLD: ["bold", "white"], YELLOW_BOLD: ["bold", "bgYellow"], MAGENTA_BOLD: ["bold", "bgMagenta"] });
 module.exports = {
-    RED_BOLD: colors.RED_BOLD,
-    BLUE_BOLD: colors.BLUE_BOLD,
-    CYAN_BOLD: colors.CYAN_BOLD,
-    WHITE_BOLD: colors.WHITE_BOLD,
-    YELLOW_BOLD: colors.YELLOW_BOLD,
-    MAGENTA_BOLD: colors.MAGENTA_BOLD,
-
-    RED: colors.red,
-    BOLD: colors.bold,
-    CYAN: colors.cyan,
-    BLUE: colors.blue,
-    GRAY: colors.gray,
-    WHITE: colors.white,
-    GREEN: colors.green,
-    YELLOW: colors.yellow,
-    MAGENTA: colors.magenta
+    GetTime, Print: (type, message) => console.log(`${GetTime()} ${type} ${message}`),
+    RED: colors.red, BOLD: colors.bold, CYAN: colors.cyan, BLUE: colors.blue, GRAY: colors.gray, WHITE: colors.white, GREEN: colors.green, YELLOW: colors.yellow, MAGENTA: colors.magenta,
+    RED_BOLD: colors.RED_BOLD, BLUE_BOLD: colors.BLUE_BOLD, CYAN_BOLD: colors.CYAN_BOLD, WHITE_BOLD: colors.WHITE_BOLD, YELLOW_BOLD: colors.YELLOW_BOLD, MAGENTA_BOLD: colors.MAGENTA_BOLD,
 };
-
-module.exports.GetTime = GetTime;
-module.exports.Print = (type, message) =>
-    console.log(`${GetTime()} ${type} ${message}`);
