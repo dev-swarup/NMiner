3adb0141ef3667c08c40ecc50bae299252dd0150
Swarup Banerjee <dev.swarup@hotmail.com>
2026-07-14 11:38:27 +0530

    refactor: update EventEmitter methods to return 'this' for better chaining

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -2,16 +2,20 @@ type EventMap = Record<string, any[]>;
 export class EventEmitter<Events extends EventMap> {
     private listeners: { [K in keyof Events]?: Array<(...args: Events[K]) => void | Promise<void>>; } = {};
 
-    public on<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void | Promise<void>): void {
+    public on<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void | Promise<void>): this {
         if (!this.listeners[event]) this.listeners[event] = [];
         this.listeners[event]!.push(listener);
+
+        return this;
     };
 
-    public off<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void | Promise<void>): void {
+    public off<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void | Promise<void>): this {
         if (!this.listeners[event])
-            return;
+            return this;
 
         this.listeners[event] = this.listeners[event]!.filter(l => l !== listener);
+
+        return this;
     };
 
     public async emit<K extends keyof Events>(event: K, ...args: Events[K]): Promise<void> {
@@ -22,12 +26,12 @@ export class EventEmitter<Events extends EventMap> {
         await Promise.all(Listeners.map(listener => listener(...args)));
     };
 
-    public once<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void | Promise<void>): void {
+    public once<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void | Promise<void>): this {
         const onceWrapper = async (...args: Events[K]) => {
             this.off(event, onceWrapper);
             await listener(...args);
         };
 
-        this.on(event, onceWrapper);
+        return this.on(event, onceWrapper);
     };
 };
