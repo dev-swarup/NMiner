43538733be2b91585a256feabe892f0bae790cf7
Swarup Banerjee <dev.swarup@hotmail.com>
2026-07-16 16:41:26 +0530

    feat: update static-components.h

--- 3rdparty/hwloc/src/static-components.h
+++ 3rdparty/hwloc/src/static-components.h
@@ -2,14 +2,19 @@ HWLOC_DECLSPEC extern const struct hwloc_component hwloc_noos_component;
 HWLOC_DECLSPEC extern const struct hwloc_component hwloc_xml_component;
 HWLOC_DECLSPEC extern const struct hwloc_component hwloc_synthetic_component;
 HWLOC_DECLSPEC extern const struct hwloc_component hwloc_xml_nolibxml_component;
+#ifdef _WIN32
 HWLOC_DECLSPEC extern const struct hwloc_component hwloc_windows_component;
+#endif
+
 HWLOC_DECLSPEC extern const struct hwloc_component hwloc_x86_component;
 static const struct hwloc_component * hwloc_static_components[] = {
   &hwloc_noos_component,
   &hwloc_xml_component,
   &hwloc_synthetic_component,
   &hwloc_xml_nolibxml_component,
+#ifdef _WIN32
   &hwloc_windows_component,
+#endif
   &hwloc_x86_component,
   NULL
 };
