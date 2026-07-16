266309bc60597f88dad94d261e12267b4827af0d
Swarup Banerjee <dev.swarup@hotmail.com>
2026-07-16 18:22:50 +0530

    added support for linux (hwloc)

--- 3rdparty/hwloc/src/static-components.h
+++ 3rdparty/hwloc/src/static-components.h
@@ -4,6 +4,8 @@ HWLOC_DECLSPEC extern const struct hwloc_component hwloc_synthetic_component;
 HWLOC_DECLSPEC extern const struct hwloc_component hwloc_xml_nolibxml_component;
 #ifdef _WIN32
 HWLOC_DECLSPEC extern const struct hwloc_component hwloc_windows_component;
+#elif defined(HWLOC_LINUX_SYS)
+HWLOC_DECLSPEC extern const struct hwloc_component hwloc_linux_component;
 #endif
 
 HWLOC_DECLSPEC extern const struct hwloc_component hwloc_x86_component;
@@ -14,6 +16,8 @@ static const struct hwloc_component * hwloc_static_components[] = {
   &hwloc_xml_nolibxml_component,
 #ifdef _WIN32
   &hwloc_windows_component,
+#elif defined(HWLOC_LINUX_SYS)
+  &hwloc_linux_component,
 #endif
   &hwloc_x86_component,
   NULL
