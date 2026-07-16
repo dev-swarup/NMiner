3038594906f7ed70b3bf3ea5dd746f93c8f2306c
Swarup Banerjee <dev.swarup@hotmail.com>
2026-07-16 11:06:38 +0530

    feat: refactor EventEmitter to improve type safety and listener management

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -1,37 +1,41 @@
 type EventMap = Record<string, any[]>;
 export class EventEmitter<Events extends EventMap> {
-    private listeners: { [K in keyof Events]?: Array<(...args: Events[K]) => void | Promise<void>>; } = {};
+    private listeners: { [key: string]: Array<(...args: any[]) => void | Promise<void>> } = Object.create(null);
 
     public on<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void | Promise<void>): this {
-        if (!this.listeners[event]) this.listeners[event] = [];
-        this.listeners[event]!.push(listener);
+        const k = event as string;
+        if (!this.listeners[k]) this.listeners[k] = [];
 
+        this.listeners[k].push(listener);
         return this;
     };
 
     public off<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void | Promise<void>): this {
-        if (!this.listeners[event])
-            return this;
+        const k = event as string;
+        const list = this.listeners[k];
 
-        this.listeners[event] = this.listeners[event]!.filter(l => l !== listener);
+        if (!list) return this;
+
+        const idx = list.indexOf(listener as any);
+        if (idx !== -1) list.splice(idx, 1);
 
         return this;
     };
 
     public async emit<K extends keyof Events>(event: K, ...args: Events[K]): Promise<void> {
-        if (!this.listeners[event])
-            return;
+        const k = event as string;
+        const list = this.listeners[k];
+        if (!list || list.length === 0) return;
 
-        const Listeners = [...this.listeners[event]!];
-        await Promise.all(Listeners.map(listener => listener(...args)));
+        await Promise.all(list.map(l => l(...args)));
     };
 
     public once<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void | Promise<void>): this {
         const onceWrapper = async (...args: Events[K]) => {
-            this.off(event, onceWrapper);
+            this.off(event, onceWrapper as any);
             await listener(...args);
         };
 
-        return this.on(event, onceWrapper);
+        return this.on(event, onceWrapper as any);
     };
 };
