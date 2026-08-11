d80da8b647a4c030f0188292f4ea7c57f4a709f7
Swarup Banerjee <dev.swarup@hotmail.com>
2026-08-11 11:35:15 +0530

    feat: updated RxNodeCache and RxCacheInfo types

--- src/js/miner.d.ts
+++ src/js/miner.d.ts
@@ -5,14 +5,34 @@ declare type JobResult = {
     txnCount?: number;
 }
 
-declare type RxVariant = "rx/0" | "rx/monero";
+declare type RxVariant = "rx/0" | "rx/monero" | "rx/v2";
 declare type RxSubmitFn = (nonce: Buffer, result: Buffer) => void | Promise<void>
+
+declare type RxNodeCache = {
+    node: number;
+    l2: number;
+    l3: number;
+    cores: number;
+    pus: number;
+    threads: number;
+}
+
+declare type RxCacheInfo = {
+    l2PerThread: number;
+    l3PerThread: number;
+    blake2: "avx2" | "sse41" | "reference";
+    nodes: RxNodeCache[];
+}
 
 export class Rx {
     constructor(variant: RxVariant, mode: RxMode);
 
+    public readonly variant: RxVariant;
+
     public allocate(seed_hash: Buffer): Promise<boolean>;
     public reallocate(seed_hash: Buffer, variant?: RxVariant): Promise<boolean>;
+
+    public hash(input: Buffer): Buffer;
 }
 
 export class RxJob {
@@ -22,10 +42,14 @@ export class RxJob {
     public pause(): void;
     public start(threads?: number[]): void;
     public throttle(threads: number, ms: number): void;
-    
+
     public get_hashes(): number;
     public send_job(blob: Buffer, diff: Buffer, nicehash: boolean, reset_nonce: boolean, start_nonce?: number, nonce_limit?: number): JobResult;
 }
 
 export function numaNodes(): number;
 export function hugePages(pages?: number): number;
+
+export function cacheInfo(): RxCacheInfo;
+
+export function recommendedThreads(): number[];
